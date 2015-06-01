3c0d9a570186a301497aa2b0382896781b0c2527
Mathias Dalheimer <md@gonium.net>
2015-06-01 17:37:42 +0200

    feeding all measurements into mqtt

--- mqtt.go
+++ mqtt.go
@@ -29,19 +29,35 @@ func NewMQTTSubmitter(ds ReadingChannel, cc ControlChannel) (*MQTTSubmitter, err
 	}
 }
 
+func (ms *MQTTSubmitter) submitReading(basechannel string,
+	subchannel string, reading float32) {
+	payload := fmt.Sprintf("%f", reading)
+	channel := fmt.Sprintf("%s/%s", basechannel, subchannel)
+	token := ms.mqtt.Publish(channel, 0, false, payload)
+	token.Wait()
+	if token.Error() != nil {
+		fmt.Printf("Error: >%s< while submitting %s\r\n", token.Error().Error(), payload)
+	}
+}
+
 func (ms *MQTTSubmitter) ConsumeData() {
 	for {
 		// TODO: Read on control, terminate goroutine when
 		readings := <-ms.datastream
-		payload := fmt.Sprintf("%s", &readings)
-		channel := "SDM630/foo"
-		// TODO: Fan out and publish individual sensor readings on separate
-		// channels.
-		token := ms.mqtt.Publish(channel, 0, false, payload)
-		token.Wait()
-		if token.Error() != nil {
-			fmt.Printf("Error: >%s< while submitting %s\r\n", token.Error().Error(), payload)
-		}
+		basechannel := "SDM630/foo"
+		ms.submitReading(basechannel, "L1/Voltage", readings.L1Voltage)
+		ms.submitReading(basechannel, "L2/Voltage", readings.L2Voltage)
+		ms.submitReading(basechannel, "L3/Voltage", readings.L3Voltage)
+		ms.submitReading(basechannel, "L1/Current", readings.L1Current)
+		ms.submitReading(basechannel, "L2/Current", readings.L2Current)
+		ms.submitReading(basechannel, "L3/Current", readings.L3Current)
+		ms.submitReading(basechannel, "L1/Power", readings.L1Power)
+		ms.submitReading(basechannel, "L2/Power", readings.L2Power)
+		ms.submitReading(basechannel, "L3/Power", readings.L3Power)
+		ms.submitReading(basechannel, "L1/CosPhi", readings.L1CosPhi)
+		ms.submitReading(basechannel, "L2/CosPhi", readings.L2CosPhi)
+		ms.submitReading(basechannel, "L3/CosPhi", readings.L3CosPhi)
+
 	}
 	ms.mqtt.Disconnect(250)
 }
